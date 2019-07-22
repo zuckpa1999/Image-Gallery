bfda890e59a55ea4dffd81fcaff27d498c8927ad
phupaa <zuckpa1999@gmail.com>
2019-07-22 17:04:51 +0700

    1st commit pic

---
+++ pics/src/component/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <form className="ui form">
+        <div className="field">
+          <label>Image Search</label>
+          <input text="push" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
