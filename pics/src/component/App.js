909c4f2e9180b0fd8f456f2d0ff97283955adf15
phupaa <zuckpa1999@gmail.com>
2019-07-23 15:39:39 +0700

    pic fetch api

--- pics/src/component/App.js
+++ pics/src/component/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import axios from "axios";
 import SearchBar from "./SearchBar";
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 17556482ff0020535f278f94af188bd49240691e19ef482a3c8e0c2f9d2931ec"
+      }
+    });
+    console.log(response.data.results);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- pics/src/component/SearchBar.js
+++ pics/src/component/SearchBar.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const SearchBar = () => {
-  return (
-    <div>
-      <form className="ui form">
-        <div className="field">
-          <label>Image Search</label>
-          <input text="push" />
-        </div>
-      </form>
-    </div>
-  );
-};
+class SearchBar extends React.Component {
+  state = { term: "" };
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+    console.log("fee");
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            {/* call back fnw/o () method */}
+            <input
+              text="push"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
